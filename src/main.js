d6bb8bdb77876656673e427615d51d97332dca47
Awen <2889984509@qq.com>
2021-11-18 11:55:43 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./css/index.css";
+import "./styles/index.less"; // 全局样式
+import Vant from "vant"; // vant组件库
+import "vant/lib/index.css"; // vant样式
 Vue.config.productionTip = false;
-
+Vue.use(Vant); // 注册使用vant组件库
 new Vue({
   router,
   store,
